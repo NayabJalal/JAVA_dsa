d9e7e01d862737e21b765ed3066dc4bc1f874438
NayabJalal <jmd341602@gmail.com>
2025-11-04 17:45:54 +0530

    BinaryBasic

---
+++ dsaJava/search/BinarySearch.java
@@ -0,0 +1,19 @@
+package search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2,3,7,9,12,19,23,43,67,78,98,99};
+        int target = 17;
+        System.out.println(binary(arr,target));
+    }
+    static int binary(int[] arr, int target){
+        int start=0, end = arr.length-1;
+        while (start<=end){
+            int mid = (start+end)/2;
+            if(target==arr[mid]) return mid;
+            if (target>arr[mid]) start=mid+1;
+            else end=mid-1;
+        }
+        return -1;
+    }
+}
