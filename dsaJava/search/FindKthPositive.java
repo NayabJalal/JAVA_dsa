630c861394f1d3c69cff7335ab1a5d168c4d469c
NayabJalal <jmd341602@gmail.com>
2025-11-17 00:23:25 +0530

    :(

---
+++ dsaJava/search/FindKthPositive.java
@@ -0,0 +1,19 @@
+package search;
+
+public class FindKthPositive {
+    public static void main(String[] args) {
+        
+    }
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + k;
+    }
+}
