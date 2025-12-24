6bb750880bb3d366e5f90c6492b526a24c2d80ab
NayabJalal <jmd341602@gmail.com>
2025-12-24 22:03:45 +0530

    :)

---
+++ dsaJava/Patterns/TwoPointers/SquaresofSortedArray.java
@@ -0,0 +1,25 @@
+package Patterns.TwoPointers;
+
+public class SquaresofSortedArray {
+    public static void main(String[] args) {
+
+    }
+    public int[] sortedSquares(int[] nums) {
+        int res[] = new int[nums.length];
+        int left=0,
+                right = nums.length-1;
+        int k=nums.length-1;
+        while (left<=right){
+            if (Math.abs(nums[left])>Math.abs(nums[right])){
+                res[k] = nums[left] * nums[left];
+                left++;
+            }
+            else {
+                res[k] = nums[right] * nums[right];
+                right--;
+            }
+            k--;
+        }
+        return res;
+    }
+}
